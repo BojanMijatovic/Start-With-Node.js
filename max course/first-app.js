a3cbdcb0b829bc3993fbf5aa6f417ef575d699d5
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-04-07 14:35:25 +0200

    start with express

---
+++ max course/first-app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`Here from log`);
+  next();
+})
+
+app.use((req, res, next) => {
+  res.send('<h1>Hello form Express </h1>');
+  next();
+})
+
+
+
+app.listen(3000);
